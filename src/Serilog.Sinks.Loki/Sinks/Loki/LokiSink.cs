bb1a197c4cc6bbd3ef9c82837a956331337147f8
Mykhailo Shevchuk <myte@ukr.net>
2020-05-13 23:03:17 +0300

    Initial project

---
+++ src/Serilog.Sinks.Loki/Sinks/Loki/LokiSink.cs
@@ -0,0 +1,13 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog.Sinks.Loki.Sinks.Loki
+{
+    public class LokiSink : ILogEventSink
+    {
+        public void Emit(LogEvent logEvent)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
