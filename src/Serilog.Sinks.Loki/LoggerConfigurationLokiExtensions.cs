b54d3ccd896f7ac46de09f6015737cdb1e9f5f4b
Mykhailo Shevchuk <myte@ukr.net>
2020-05-15 01:13:09 +0300

    Fixed typo, which broke all

--- src/Serilog.Sinks.Loki/LoggerConfigurationLokiExtensions.cs
+++ src/Serilog.Sinks.Loki/LoggerConfigurationLokiExtensions.cs
@@ -50,7 +50,7 @@ namespace Serilog.Sinks.Loki
                 formatter,
                 batchFormatter,
                 restrictedToMinimumLevel,
-                httpClient);
+                client);
         }
     }
 }
