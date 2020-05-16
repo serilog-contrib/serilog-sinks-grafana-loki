c94ab5e1c47e669ce7251cf1f44b099bf0ae342e
Mykhailo Shevchuk <myte@ukr.net>
2020-05-16 18:21:53 +0300

    Methods for settuping durable sinks

--- src/Serilog.Sinks.Loki/LoggerConfigurationLokiExtensions.cs
+++ src/Serilog.Sinks.Loki/LoggerConfigurationLokiExtensions.cs
@@ -33,14 +33,7 @@ namespace Serilog.Sinks.Loki
                 throw new ArgumentNullException(nameof(sinkConfiguration));
             }
 
-            var batchFormatter = labels != null ? new LokiBatchFormatter(labels) : new LokiBatchFormatter();
-            var formatter = textFormatter ?? new MessageTemplateTextFormatter(outputTemplate);
-            var client = httpClient ?? new DefaultLokiHttpClient();
-
-            if (client is ILokiHttpClient lokiHttpClient)
-            {
-                lokiHttpClient.SetCredentials(credentials);
-            }
+            var (batchFormatter, formatter, client) = SetupClientAndFormatters(labels, textFormatter, outputTemplate, httpClient, credentials);
 
             return sinkConfiguration.Http(
                 LokiRoutes.BuildLogsEntriesRoute(url),
@@ -51,6 +44,101 @@ namespace Serilog.Sinks.Loki
                 batchFormatter,
                 restrictedToMinimumLevel,
                 client);
+        }
+
+        public static LoggerConfiguration DurableLokiUsingTimeRolledBuffers(
+            this LoggerSinkConfiguration sinkConfiguration,
+            string url,
+            string bufferPathFormat = "Buffer-{Date}.json",
+            long? bufferFileSizeLimitBytes = null,
+            bool bufferFileShared = false,
+            int? retainedBufferFileCountLimit = 31,
+            IEnumerable<LokiLabel> labels = null,
+            LokiCredentials credentials = null,
+            string outputTemplate = DefaultOutputTemplate,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            int batchPostingLimit = 1000,
+            int? queueLimit = null,
+            TimeSpan? period = null,
+            ITextFormatter textFormatter = null,
+            IHttpClient httpClient = null)
+        {
+            if (sinkConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(sinkConfiguration));
+            }
+
+            var (batchFormatter, formatter, client) = SetupClientAndFormatters(labels, textFormatter, outputTemplate, httpClient, credentials);
+
+            return sinkConfiguration.DurableHttpUsingTimeRolledBuffers(
+                LokiRoutes.BuildLogsEntriesRoute(url),
+                bufferPathFormat,
+                bufferFileSizeLimitBytes,
+                bufferFileShared,
+                retainedBufferFileCountLimit,
+                batchPostingLimit,
+                period,
+                formatter,
+                batchFormatter,
+                restrictedToMinimumLevel,
+                client);
+        }
+
+        public static LoggerConfiguration DurableLokiUsingFileSizeRolledBuffers(
+            this LoggerSinkConfiguration sinkConfiguration,
+            string url,
+            string bufferBaseFileName = "Buffer",
+            long? bufferFileSizeLimitBytes = 1024 * 1024 * 1024,
+            bool bufferFileShared = false,
+            int? retainedBufferFileCountLimit = 31,
+            IEnumerable<LokiLabel> labels = null,
+            LokiCredentials credentials = null,
+            string outputTemplate = DefaultOutputTemplate,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            int batchPostingLimit = 1000,
+            int? queueLimit = null,
+            TimeSpan? period = null,
+            ITextFormatter textFormatter = null,
+            IHttpClient httpClient = null)
+        {
+            if (sinkConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(sinkConfiguration));
+            }
+
+            var (batchFormatter, formatter, client) = SetupClientAndFormatters(labels, textFormatter, outputTemplate, httpClient, credentials);
+
+            return sinkConfiguration.DurableHttpUsingFileSizeRolledBuffers(
+                LokiRoutes.BuildLogsEntriesRoute(url),
+                bufferBaseFileName,
+                bufferFileSizeLimitBytes,
+                bufferFileShared,
+                retainedBufferFileCountLimit,
+                batchPostingLimit,
+                period,
+                formatter,
+                batchFormatter,
+                restrictedToMinimumLevel,
+                client);
+        }
+
+        private static (IBatchFormatter batchFormatter, ITextFormatter textFormatter, IHttpClient httpClient) SetupClientAndFormatters(
+            IEnumerable<LokiLabel> labels,
+            ITextFormatter textFormatter,
+            string outputTemplate,
+            IHttpClient httpClient,
+            LokiCredentials credentials)
+        {
+            var batchFormatter = labels != null ? new LokiBatchFormatter(labels) : new LokiBatchFormatter();
+            var formatter = textFormatter ?? new MessageTemplateTextFormatter(outputTemplate);
+            var client = httpClient ?? new DefaultLokiHttpClient();
+
+            if (client is ILokiHttpClient lokiHttpClient)
+            {
+                lokiHttpClient.SetCredentials(credentials);
+            }
+
+            return ((IBatchFormatter) batchFormatter, formatter, client);
         }
     }
 }
