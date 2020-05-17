fa756130e7ef3443dcb37b92ed342cf7534189d2
Mykhailo Shevchuk <myte@ukr.net>
2020-05-17 15:54:12 +0300

    Added lisense header to code files

--- src/Serilog.Sinks.Grafana.Loki/Utils/LogEventLevelExtensions.cs
+++ src/Serilog.Sinks.Grafana.Loki/Utils/LogEventLevelExtensions.cs
@@ -1,4 +1,14 @@
-﻿using Serilog.Events;
+﻿// Copyright 2020 Mykhailo Shevchuk & Contributors
+//
+// Licensed under the MIT license;
+// you may not use this file except in compliance with the License.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See LICENSE file in the project root for full license information.
+
+using Serilog.Events;
 
 namespace Serilog.Sinks.Grafana.Loki.Utils
 {
