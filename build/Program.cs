6b02a9d2dee7cd34a47ee81a6373053fb5211536
Mykhailo Shevchuk <myte@ukr.net>
2021-05-10 18:41:55 +0300

    New GH flows + build with bullseye

---
+++ build/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using static Bullseye.Targets;
+using static SimpleExec.Command;
+
+namespace Build
+{
+    internal static class Program
+    {
+        private const string PackOutput = "./artifacts";
+        private const string Solution = "Serilog.Sinks.Grafana.Loki.sln";
+
+        internal static void Main(string[] args)
+        {
+            Target(Targets.CleanBuildOutput, () => { Run("dotnet", $"clean {Solution} -c Release -v m --nologo"); });
+
+            Target(Targets.Build, DependsOn(Targets.CleanBuildOutput), () =>
+            {
+                Run("dotnet", $"build {Solution} -c Release --nologo");
+            });
+
+            Target(Targets.Test, DependsOn(Targets.Build), () =>
+            {
+                Run("dotnet", $"test {Solution} -c Release --no-build --nologo");
+            });
+
+            Target(Targets.CleanPackOutput, () =>
+            {
+                if (Directory.Exists(PackOutput))
+                {
+                    Directory.Delete(PackOutput, true);
+                }
+            });
+
+            Target(Targets.Pack, DependsOn(Targets.CleanPackOutput), () =>
+            {
+                Run("dotnet", $"pack ./src/Serilog.Sinks.Grafana.Loki/Serilog.Sinks.Grafana.Loki.csproj -c Release -o {Directory.CreateDirectory(PackOutput).FullName} --no-build --nologo");
+            });
+
+            Target("default", DependsOn(Targets.Test, Targets.Pack));
+
+            RunTargetsAndExit(args);
+        }
+    }
+}
---
+++ build/Targets.cs
@@ -0,0 +1,11 @@
+﻿namespace Build
+{
+    internal static class Targets
+    {
+        internal const string Build = "build";
+        internal const string CleanBuildOutput = "clean-build-output";
+        internal const string CleanPackOutput = "clean-pack-output";
+        internal const string Pack = "pack";
+        internal const string Test = "test";
+    }
+}
