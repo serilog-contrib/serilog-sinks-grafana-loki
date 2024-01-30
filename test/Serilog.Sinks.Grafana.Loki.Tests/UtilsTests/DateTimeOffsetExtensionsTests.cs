4f90718e1870bfe0c5b3f4135345bb49e6357642
Mykhailo Shevchuk <myte@ukr.net>
2024-01-31 00:18:08 +0200

    Add nanosecond support for timestamps for versions prior to .NET 7 (#249)
    * Change functions for using ticks
    
    * Adjusted CI for multiple dotnet versions
    
    * Fix test names + added missed test
    
    * Fix pipeline
    
    * CI
    
    * Removed netstandard2.0 from the test project

--- test/Serilog.Sinks.Grafana.Loki.Tests/UtilsTests/DateTimeOffsetExtensionsTests.cs
+++ test/Serilog.Sinks.Grafana.Loki.Tests/UtilsTests/DateTimeOffsetExtensionsTests.cs
@@ -6,6 +6,7 @@ namespace Serilog.Sinks.Grafana.Loki.Tests.UtilsTests;
 
 public class DateTimeOffsetExtensionsTests
 {
+    #if NET7_0_OR_GREATER
     [Fact]
     public void UnixEpochShouldBeConvertedCorrectly()
     {
@@ -17,25 +18,39 @@ public class DateTimeOffsetExtensionsTests
     }
 
     [Fact]
-    public void DateTimeOffsetShouldBeConvertedCorrectly()
-    {
-        var dateTimeOffset = new DateTimeOffset(2021, 05, 25, 12, 00, 00, TimeSpan.Zero);
-
-        var result = dateTimeOffset.ToUnixNanosecondsString();
-
-        result.ShouldBe("1621944000000000000");
-    }
-
-#if NET7_0_OR_GREATER
-    [Fact]
     public void DateTimeNanosecondsOffsetShouldBeConvertedCorrectly()
     {
-        var dateTimeOffset =
-            new DateTimeOffset(2021, 05, 25, 12, 00, 00, 777, 888, TimeSpan.Zero).AddMicroseconds(0.999); // There is no other way to set nanoseconds
+        var dateTimeOffset = new DateTimeOffset(2021, 05, 25, 12, 00, 00, 777, 888, TimeSpan.Zero).AddMicroseconds(0.999); // There is no other way to set nanoseconds
 
         var result = dateTimeOffset.ToUnixNanosecondsString();
 
         result.ShouldBe("1621944000777888900");
     }
-#endif
+
+    #else
+    [Fact]
+    public void UnixEpochShouldBeConvertedCorrectly()
+    {
+        var epoch = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+        var result = epoch.ToUnixNanosecondsString();
+
+        result.ShouldBe("0");
+    }
+
+    [Fact]
+    public void DateTimeNanosecondsOffsetShouldBeConvertedCorrectly()
+    {
+        const long nanosecondsPerTick = 100;
+
+        var ticks = new DateTimeOffset(2021, 05, 25, 12, 00, 00, TimeSpan.Zero).Ticks;
+        ticks += 777888999 / nanosecondsPerTick;
+
+        var dateTimeOffset = new DateTimeOffset(ticks, TimeSpan.Zero);
+
+        var result = dateTimeOffset.ToUnixNanosecondsString();
+
+        result.ShouldBe("1621944000777888900");
+    }
+    #endif
 }
